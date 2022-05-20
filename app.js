a28a3e4755bc5d265f51d4075f99839f4418d5ff
Hamza <sanshinehamza@gmail.com>
2022-05-20 19:10:00 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
+      window.scrollTo(0, 0);
+    });
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+})();
+
+// import Typed from "typed.js";
+
+// var options = {
+//   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+//   typeSpeed: 40,
+// };
+
+// var typed = new Typed(".element", options);
